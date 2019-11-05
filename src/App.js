11149f9a50540f8bfc830cc78e1585fbd89a4b3d
Viktor <anvictor0@gmail.com>
2019-11-05 20:19:18 +0200

    fetch done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true};
+    this.question = {
+      head: "https://api.github.com/search/users?q=location%3A",
+      middle: "+followers%3A%3E%3D",
+      tail: "&type=Users"
+    };
+    this.location = "kiev";
+    this.followers = "200";
+  }
+
+  componentDidMount(){
+    return fetch(`${this.question.head}${this.location}${this.question.middle}${this.followers}${this.question.tail}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+        console.log(this.state);
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+  render(){
+    if(this.state.isLoading){
+      return(
+        <div className="App">
+          <header className="App-header">
+            <p>
+              {"loading"}
+            </p>
+
+          </header>
+        </div>
+      )
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            {this.state.dataSource.items[3].login}
+          </p>
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
